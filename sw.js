72ba902515e3c4e3dbc7a1726bfaf2f6b509e9b0
kellynwong <lookingforstars@hotmail.com>
2026-09-14 22:39:48 +0800

    Fix swipe jumping to Home

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "study-ai-v14";
+const CACHE = "study-ai-v15";
 const ASSETS = ["./index.html", "./cards.js", "./viz.js", "./manifest.json", "./apple-touch-icon.png", "./icon-192.png", "./icon-512.png", "./favicon.png", "./"];
 
 self.addEventListener("install", (event) => {
